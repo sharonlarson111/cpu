b655f5e697d20d68559e6596ab051915cd16e89c
sharonlarson111 <77948284+sharonlarson111@users.noreply.github.com>
2021-03-14 09:03:13 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qz8qwl3a0wrv8cpjdfwls59n2mefdmwtz3uqzf5.eva -t10
 
 
 done
